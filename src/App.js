7ce7d91076bedd10de440516dd5a6d9dae57efbe
Thomas Chapeaux <chapeauxthomas@gmail.com>
2019-06-17 18:23:49 +0200

    Initial commit with basic logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Timer, TimerView } from "./Timer.jsx";
+
+import "./App.css";
+
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timers: []
+    };
+  }
+
+  onAddTimer = () => {
+    this.setState(prevState => ({
+      timers: [...prevState.timers, new Timer(Date.now())]
+    }));
+  };
+
+  render() {
+    const { timers } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {timers.map((t, idx) => (
+            <TimerView key={idx} timer={t} />
+          ))}
+          <button onClick={this.onAddTimer}>Add timer</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Timer.jsx
@@ -0,0 +1,110 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+export class Timer {
+  constructor(startTime) {
+    this.startTime = startTime;
+    this.accumulatedTime = 0;
+    this.running = false;
+  }
+
+  hasStarted() {
+    return this.running || this.accumulatedTime > 0;
+  }
+
+  getCurrentTime() {
+    return (
+      this.accumulatedTime + (this.running ? Date.now() - this.startTime : 0)
+    );
+  }
+
+  pause() {
+    this.accumulatedTime += Date.now() - this.startTime;
+    this.startTime = -1;
+    this.running = false;
+  }
+
+  resume() {
+    this.startTime = Date.now();
+    this.running = true;
+  }
+
+  reset() {
+    this.startTime = -1;
+    this.accumulatedTime = 0;
+    this.running = false;
+  }
+}
+
+const MS_IN_SECONDS = 1000;
+const MS_IN_MINUTES = 1000 * 60;
+const MS_IN_HOUR = 1000 * 60 * 60;
+
+export class TimerView extends PureComponent {
+  static propTypes = {
+    timer: PropTypes.instanceOf(Timer).isRequired
+  };
+
+  constructor() {
+    super();
+
+    const frame = () => {
+      this.forceUpdate();
+      this._animationframe = window.requestAnimationFrame(frame);
+    };
+
+    this._animationframe = window.requestAnimationFrame(frame);
+  }
+
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this._animationframe);
+  }
+
+  onToggleRunning(timer) {
+    if (timer.running) {
+      timer.pause();
+    } else {
+      timer.resume();
+    }
+  }
+
+  render() {
+    const { timer } = this.props;
+
+    const ms = timer.getCurrentTime();
+
+    const hours = Math.floor(ms / MS_IN_HOUR);
+    const minutes = Math.floor((ms - hours * MS_IN_HOUR) / MS_IN_MINUTES);
+    const seconds = Math.floor(
+      (ms - hours * MS_IN_HOUR - minutes * MS_IN_MINUTES) / MS_IN_SECONDS
+    );
+
+    const hh = String(hours).padStart(2, 0);
+    const mm = String(minutes).padStart(2, 0);
+    const ss = String(seconds).padStart(2, 0);
+
+    const classes = classNames("timer", {
+      "timer-running": timer.running
+    });
+
+    return (
+      <div className={classes}>
+        <span className="timer-time">{`${hh}:${mm}:${ss}`}</span>
+        {
+          <button
+            className="button-toggle"
+            onClick={this.onToggleRunning.bind(null, timer)}
+          >
+            <i className={timer.running ? "fa fa-pause" : "fa fa-play"} />
+          </button>
+        }
+        {timer.hasStarted() ? (
+          <button className="button-stop" onClick={() => timer.reset()}>
+            <i className="fa fa-stop" />
+          </button>
+        ) : null}
+      </div>
+    );
+  }
+}
